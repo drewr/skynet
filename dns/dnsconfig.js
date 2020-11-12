014ea63e18d82afc59ee157e4d7eeebca7247dbf
Drew Raines <drew@raines.me>
2020-11-12 14:06:30 -0600

    Add GCLOUD dnscontrol

--- dns/dnsconfig.js
+++ dns/dnsconfig.js
@@ -1,6 +1,11 @@
-var REG_NAMECOM = NewRegistrar("name.com","NAMEDOTCOM");
-var GCLOUD = NewDnsProvider("gcloud", "GCLOUD");
+var REG_NONE = NewRegistrar('none', 'NONE')
+var DNS_GCLOUD = NewDnsProvider("gcloud", "GCLOUD");
+var DNS_BIND = NewDnsProvider('bind', 'BIND');  // ISC BIND.
 
-D("example.tld", REG_NAMECOM, DnsProvider(GCLOUD),
+D("example.com", REG_NONE, DnsProvider(DNS_BIND),
+    A("test","1.2.3.4")
+);
+
+D("draines.com", REG_NONE, DnsProvider(DNS_GCLOUD),
     A("test","1.2.3.4")
 );
